30f7c56a7c6248d4a5198402460f503dce26c655
schmidt-annika <135436065+schmidt-annika@users.noreply.github.com>
2023-06-04 21:23:22 +0200

    Uploaded initial yet working version of astrorallye

---
+++ script.js
@@ -0,0 +1,456 @@
+var passwort = "LesBar-Crew";
+
+var frage1 = "Frage 1.1";
+var antwort1 = "Antwort 1.1";
+
+var frage2 = "Frage 1.2";
+var antwort2 = "Antwort 1.2";
+
+var frage3 = "Frage 1.3";
+var antwort3 = "Antwort 1.3";
+
+var frage4 = "Frage 1.4";
+var antwort4 = "Antwort 1.4";
+
+var frage5 = "Frage 1.5";
+var antwort5 = "Antwort 1.5";
+
+var frage6 = "Frage 1.6";
+var antwort6 = "Antwort 1.6";
+
+var frage7 = "Frage 1.7";
+var antwort7 = "Antwort 1.7";
+
+var frage8 = "Frage 1.8";
+var antwort8 = "Antwort 1.8";
+
+var frage9 = "Frage 1.9";
+var antwort9 = "Antwort 1.9";
+
+var frage10 = "Frage 1.10";
+var antwort10 = "Antwort 1.10";
+
+var frage11 = "Frage 1.11";
+var antwort11 = "Antwort 1.11";
+
+var frage12 = "Frage 1.12";
+var antwort12 = "Antwort 1.12";
+
+var frage13 = "Frage 1.13";
+var antwort13 = "Antwort 1.13";
+
+var frage14 = "Frage 1.14";
+var antwort14 = "Antwort 1.14";
+
+var frage15 = "Frage 1.15";
+var antwort15 = "Antwort 1.15";
+
+var frage16 = "Frage 2.1";
+var antwort16 = "Antwort 2.1";
+
+var frage17 = "Frage 2.2";
+var antwort17 = "Antwort 2.2";
+
+var frage18 = "Frage 2.3";
+var antwort18 = "Antwort 2.3";
+
+var frage19 = "Frage 2.4";
+var antwort19 = "Antwort 2.4";
+
+var frage20 = "Frage 2.5";
+var antwort20 = "Antwort 2.5";
+
+var frage21 = "Frage 2.6";
+var antwort21 = "Antwort 2.6";
+
+var frage22 = "Frage 2.7";
+var antwort22 = "Antwort 2.7";
+
+var frage23 = "Frage 2.8";
+var antwort23 = "Antwort 2.8";
+
+var frage24 = "Frage 2.9";
+var antwort24 = "Antwort 2.9";
+
+var frage25 = "Frage 2.10";
+var antwort25 = "Antwort 2.10";
+
+var frage26 = "Frage 2.11";
+var antwort26 = "Antwort 2.11";
+
+var frage27 = "Frage 2.12";
+var antwort27 = "Antwort 2.12";
+
+var frage28 = "Frage 2.13";
+var antwort28 = "Antwort 2.13";
+
+var frage29 = "Frage 2.14";
+var antwort29 = "Antwort 2.14";
+
+var frage30 = "Frage 2.15";
+var antwort30 = "Antwort 2.15";
+
+var questionCounter = 0;
+
+var questions1 = [
+	frage1, frage2, frage3, frage4, frage5, frage6, frage7, frage8, frage9, frage10, frage11, frage12, frage13, frage14, frage15
+];
+var answers1 = [
+	antwort1, antwort2, antwort3, antwort4, antwort5, antwort6, antwort7, antwort8, antwort9, antwort10, antwort11, antwort12, antwort13, antwort14, antwort15
+];
+
+var questions2 = [
+	frage17, frage18, frage19, frage20, frage21, frage22, frage23, frage24, frage25, frage26, frage27, frage28, frage29, frage30, frage16
+];
+var answers2 = [
+	antwort17, antwort18, antwort19, antwort20, antwort21, antwort22, antwort23, antwort24, antwort25, antwort26, antwort27, antwort28, antwort29, antwort30, antwort16
+];
+
+var questions3 = [
+    frage3, frage4, frage5, frage6, frage7, frage8, frage9, frage10, frage11, frage12, frage13, frage14, frage15, frage1, frage2
+];
+var answers3 = [
+    antwort3, antwort4, antwort5, antwort6, antwort7, antwort8, antwort9, antwort10, antwort11, antwort12, antwort13, antwort14, antwort15, antwort1, antwort2
+];
+
+var questions4 = [
+    frage19, frage20, frage21, frage22, frage23, frage24, frage25, frage26, frage27, frage28, frage29, frage30, frage16, frage17, frage18
+];
+var answers4 = [
+    antwort19, antwort20, antwort21, antwort22, antwort23, antwort24, antwort25, antwort26, antwort27, antwort28, antwort29, antwort30, antwort16, antwort17, antwort18
+];
+
+var questions5 = [
+    frage5, frage6, frage7, frage8, frage9, frage10, frage11, frage12, frage13, frage14, frage15, frage1, frage2, frage3, frage4
+];
+var answers5 = [
+    antwort5, antwort6, antwort7, antwort8, antwort9, antwort10, antwort11, antwort12, antwort13, antwort14, antwort15, antwort1, antwort2, antwort3, antwort4
+];
+
+var questions6 = [
+    frage21, frage22, frage23, frage24, frage25, frage26, frage27, frage28, frage29, frage30, frage16, frage17, frage18, frage19, frage20
+];
+var answers6 = [
+    antwort21, antwort22, antwort23, antwort24, antwort25, antwort26, antwort27, antwort28, antwort29, antwort30, antwort16, antwort17, antwort18, antwort19, antwort20
+];
+
+var questions7 = [
+    frage7, frage8, frage9, frage10, frage11, frage12, frage13, frage14, frage15, frage1, frage2, frage3, frage4, frage5, frage6
+];
+var answers7 = [
+    antwort7, antwort8, antwort9, antwort10, antwort11, antwort12, antwort13, antwort14, antwort15, antwort1, antwort2, antwort3, antwort4, antwort5, antwort6
+];
+
+var questions8 = [
+    frage23, frage24, frage25, frage26, frage27, frage28, frage29, frage30, frage16, frage17, frage18, frage19, frage20, frage21, frage22
+];
+var answers8 = [
+    antwort23, antwort24, antwort25, antwort26, antwort27, antwort28, antwort29, antwort30, antwort16, antwort17, antwort18, antwort19, antwort20, antwort21, antwort22
+];
+
+var questions9 = [
+    frage9, frage10, frage11, frage12, frage13, frage14, frage15, frage1, frage2, frage3, frage4, frage5, frage6, frage7, frage8
+];
+var answers9 = [
+    antwort9, antwort10, antwort11, antwort12, antwort13, antwort14, antwort15, antwort1, antwort2, antwort3, antwort4, antwort5, antwort6, antwort7, antwort8
+];
+
+var questions10 = [
+    frage25, frage26, frage27, frage28, frage29, frage30, frage16, frage17, frage18, frage19, frage20, frage21, frage22, frage23, frage24
+];
+var answers10 = [
+    antwort25, antwort26, antwort27, antwort28, antwort29, antwort30, antwort16, antwort17, antwort18, antwort19, antwort20, antwort21, antwort22, antwort23, antwort24
+];
+
+var questions11 = [
+    frage11, frage12, frage13, frage14, frage15, frage1, frage2, frage3, frage4, frage5, frage6, frage7, frage8, frage9, frage10
+];
+var answers11 = [
+    antwort11, antwort12, antwort13, antwort14, antwort15, antwort1, antwort2, antwort3, antwort4, antwort5, antwort6, antwort7, antwort8, antwort9, antwort10
+];
+
+var questions12 = [
+    frage27, frage28, frage29, frage30, frage16, frage17, frage18, frage19, frage20, frage21, frage22, frage23, frage24, frage25, frage26
+];
+var answers12 = [
+    antwort27, antwort28, antwort29, antwort30, antwort16, antwort17, antwort18, antwort19, antwort20, antwort21, antwort22, antwort23, antwort24, antwort25, antwort26
+];
+
+var questions13 = [
+    frage13, frage14, frage15, frage1, frage2, frage3, frage4, frage5, frage6, frage7, frage8, frage9, frage10, frage11, frage12
+];
+var answers13 = [
+    antwort13, antwort14, antwort15, antwort1, antwort2, antwort3, antwort4, antwort5, antwort6, antwort7, antwort8, antwort9, antwort10, antwort11, antwort12
+];
+
+var questions14 = [
+    frage29, frage30, frage16, frage17, frage18, frage19, frage20, frage21, frage22, frage23, frage24, frage25, frage26, frage27, frage28
+];
+var answers14 = [
+    antwort29, antwort30, antwort16, antwort17, antwort18, antwort19, antwort20, antwort21, antwort22, antwort23, antwort24, antwort25, antwort26, antwort27, antwort28
+];
+
+var questions15 = [
+    frage15, frage1, frage2, frage3, frage4, frage5, frage6, frage7, frage8, frage9, frage10, frage11, frage12, frage13, frage14
+];
+var answers15 = [
+    antwort15, antwort1, antwort2, antwort3, antwort4, antwort5, antwort6, antwort7, antwort8, antwort9, antwort10, antwort11, antwort12, antwort13, antwort14
+];
+
+/*if (window.navigator.standalone == false) {
+	closeElement("index");
+	closeElement("icon");
+	openElement("anleitung");
+}*/
+
+function selectList(listNumber) {
+	question = [];
+	answers = [];
+    switch (listNumber) {
+        case 1:
+            questions = questions1;
+			answers = answers1;
+			buchstabe = "U";
+            break;
+        case 2:
+            questions = questions2;
+			answers = answers2;
+			buchstabe = "N";
+            break;
+        case 3:
+            questions = questions3;
+			answers = answers3;
+			buchstabe = "T";
+            break;
+        case 4:
+            questions = questions4;
+			answers = answers4;
+			buchstabe = "E";
+            break;
+        case 5:
+            questions = questions5;
+			answers = answers5;
+			buchstabe = "R";
+            break;
+        case 6:
+            questions = questions6;
+			answers = answers6;
+			buchstabe = "D";
+            break;
+        case 7:
+            questions = questions7;
+			answers = answers7;
+			buchstabe = "E";
+            break;
+        case 8:
+            questions = questions8;
+			answers = answers8;
+			buchstabe = "R";
+            break;
+        case 9:
+            questions = questions9;
+			answers = answers9;
+			buchstabe = "A";
+            break;
+        case 10:
+            questions = questions10;
+			answers = answers10;
+			buchstabe = "U";
+            break;
+        case 11:
+            questions = questions11;
+			answers = answers11;
+			buchstabe = "S";
+            break;
+        case 12:
+            questions = questions12;
+			answers = answers12;
+			buchstabe = "L";
+            break;
+        case 13:
+            questions = questions13;
+			answers = answers13;
+			buchstabe = "E";
+            break;
+        case 14:
+            questions = questions14;
+			answers = answers14;
+			buchstabe = "I";
+            break;			
+        case 15:
+            questions = questions15;
+			answers = answers15;
+			buchstabe = "H";
+            break;
+		default:
+			questions = questions15;
+			answers = answers15;
+			buchstabe = "E";
+			break;
+    }
+	sessionStorage.setItem('questions', JSON.stringify(questions));
+	sessionStorage.setItem('answers', JSON.stringify(answers));
+	sessionStorage.setItem('buchstabe', buchstabe);
+	closeElement("index");
+	openElement("start");
+	openElement("icon");
+}
+
+function changeQuestion() {
+	let questions = JSON.parse(sessionStorage.getItem("questions"));
+	let answers = JSON.parse(sessionStorage.getItem("answers"));
+    var questionHeading = document.getElementById("question-heading");
+    var index = questionCounter;
+	var inputField = document.getElementById("answer-input");
+	var currentQuestion = questions[index];
+	var body = document.body;
+	var imagecounter = questionCounter+1;
+	var fehlermeldung = document.getElementById('fehlermeldung2');
+	if (questionCounter == 0) {
+		questionCounter++;
+		questionHeading.textContent = "Frage 1";
+        document.getElementById("question-text").textContent = questions[0];
+		document.getElementById("frage").style.backgroundImage = "url('Frage" + imagecounter + ".png')";
+	} else if (index < questions.length) {
+		var antwort = inputField.value;
+		if (antwort == "") {
+			fehlermeldung.style.display = 'block';
+		} else if (antwort == answers[index-1]){
+			questionCounter++;
+			openrichtigbildschirm();
+			fehlermeldung.style.display = 'none';
+			questionHeading.textContent = "Frage " + questionCounter;
+			document.getElementById("question-text").textContent = currentQuestion;
+			while (imagecounter > 5) {
+				imagecounter = imagecounter - 5;
+			}
+			document.getElementById("frage").style.backgroundImage = "url('Frage" + imagecounter + ".png')";
+		} else {
+			openfalschbildschirm();
+			fehlermeldung.style.display = 'none';
+		}
+    } else {
+		closeElement("frage");
+		openElement("buchstabe");
+		spanEinsetzten();
+	}
+    inputField.value = "";
+}
+
+
+function openNav() {
+	document.getElementById("myNav").style.height = "100%";
+}
+
+
+function closeNav() {
+	abbrechen();
+	abbrechen2();
+	document.getElementById("myNav").style.height = "0%";
+}
+
+function nextPage() {
+	var inputElement = document.getElementById('name-input');
+	var crewname = inputElement.value;
+	var fehlermeldung = document.getElementById('fehlermeldung');
+	if (crewname === '') {
+		fehlermeldung.style.display = 'block';
+	} else {
+		sessionStorage.setItem('crewname', crewname);
+		closeElement("start");
+		openElement("frage");
+		fehlermeldung.style.display = 'none';
+		inputElement.value = "";
+		changeQuestion();
+	}
+}
+
+function rallyeuebrspringen() {
+	var passwortabfrage = document.getElementById('passwortabfrage');
+	passwortabfrage.style.display = 'block';
+}
+
+function beendenabfrage() {
+	var beendenabfrage = document.getElementById('beendenabfrage');
+	beendenabfrage.style.display = 'block';
+}
+
+
+function abbrechen() {
+	var passwortabfrage = document.getElementById('passwortabfrage');
+	passwortabfrage.style.display = 'none';
+	var inputElement = document.getElementById('passwort-input');
+	inputElement.value = "";
+}
+
+function abbrechen2() {
+	var beendenabfrage = document.getElementById('beendenabfrage');
+	beendenabfrage.style.display = 'none';
+}
+
+function spanEinsetzten() {
+	if (sessionStorage.getItem('crewname')) {
+		var crewname = sessionStorage.getItem('crewname');
+		var crewnameSpan = document.getElementById('crewname');
+		crewnameSpan.innerHTML = crewname;
+	} else {
+		var crewnameSpan = document.getElementById('crewname');
+		crewnameSpan.innerHTML = "Astronauten";
+	}
+	if (sessionStorage.getItem('buchstabe')) {
+		var buchstabe = sessionStorage.getItem('buchstabe');
+		var buchstabeSpan = document.getElementById('buchstabe2');
+		buchstabeSpan.innerHTML = buchstabe;
+	}	
+}
+
+function loseung() {
+	var inputElement = document.getElementById('passwort-input');
+	var passwort_input = inputElement.value;
+	if (passwort_input == passwort) {
+		closeNav();
+		closeElement("frage");
+		closeElement("start");
+		openElement("buchstabe");
+		spanEinsetzten();
+		abbrechen();
+	} else {
+		inputElement.value = "";
+	}
+}
+
+function beenden() {
+	closeNav();
+	closeElement("frage");
+	closeElement("buchstabe");
+	closeElement("start");
+	closeElement("icon");
+	openElement("index");
+	sessionStorage.clear();
+	questionCounter = 0;
+}
+
+function openfalschbildschirm() {
+	document.getElementById("falsch-bildschirm").style.width = "100%";
+}
+
+function closefalschbildschirm() {
+	document.getElementById("falsch-bildschirm").style.width = "0%";
+}
+
+function openrichtigbildschirm() {
+	document.getElementById("richtig-bildschirm").style.width = "100%";
+}
+
+function closerichtigbildschirm() {
+	document.getElementById("richtig-bildschirm").style.width = "0%";
+}
+
+
+function closeElement(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+function openElement(id) {
+  document.getElementById(id).style.display = "block";
+}
